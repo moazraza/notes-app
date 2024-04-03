2068512d57f4eab1ff8ffb8dcb15bf05c98c2c42
Moaz Raza <smmoazraza@gmail.com>
2024-04-03 12:05:57 +0800

    Added navbar and basic routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import {NgModule} from "@angular/core";
-import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
     declarations: [],
     imports: [
-        AppRoutingModule
+
     ],
     providers: [],
     bootstrap: []
