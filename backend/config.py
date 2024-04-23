dc0dac6d232a7d301a4a5612c3555a75e37d9165
vanartist <810567808@qq.com>
2024-04-23 21:52:01 +0800

    1.User Registration Interface: Added a file parameter, allowing users to add an avatar during registration.
    2.Create Comment Interface:
    Added an interface to create a comment using postId (the ID of each post in the MongoDB database) and content. The creator's information is obtained from the authorization.
    
    3.Get Comments Interface:
    Users can retrieve comments related to a post by the postId.
    
    4.Get Single Image Interface:
    Users can find an image associated with a post by the image name filename. Since the images in the post table are stored as a list, iterating through the list and using this interface with the image name retrieves the image associated with the post.
    
    5.Get Icon Interface:
    Logged-in users can view their own icon, which requires authorization.

--- backend/config.py
+++ backend/config.py
@@ -2,4 +2,3 @@ import os
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 POST_UPLOADED_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images\\notes_images")
 ICON_UPLOADED_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images\\icon")
-print(POST_UPLOADED_FILE_PATH)
